9f0aaae606a201f79009f88d04fb38753e15f134
mohdsahil0 <you@example.com>
2021-09-18 14:44:30 +0530

    Add php

---
+++ Admin/sidebar.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/admin-style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+    <title>CelloTax - Admin Panel</title>
+</head>
+
+<body>
+    <div class="main_box">
+        <input type="checkbox" id="check">
+        <div class="btn_one">
+            <label for="check">
+            <i class="fa fa-bars"></i>
+          </label>
+        </div>
+        <div class="sidebar_menu">
+            <div class="logo">
+                <a href="#">CodingLab</a>
+            </div>
+            <div class="btn_two">
+                <label for="check">
+                <i class="fa fa-times"></i>
+              </label>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><i class="fa fa-qrcode"></i>
+                        <a href="#">Dashboard</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-link"></i>
+                        <a href="#">Shortcuts</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-stream"></i>
+                        <a href="#">Overview</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-calendar-week"></i>
+                        <a href="#">Events</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-question-circle"></i>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-sliders-h"></i>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-phone-volume"></i>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-comments"></i>
+                        <a href="#">Feedback</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="social_media">
+                <ul>
+                    <a href="#"><i class="fa fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
