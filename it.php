a23ea32ed96cf192d73b59b447a20b740deacb37
FrazileDevelopers <frazile.com@gmail.com>
2021-09-18 10:49:26 +0530

    PHP Added

---
+++ it.php
@@ -0,0 +1,359 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/main.js">
+    </script>
+    <title>CelloTax - Income Tax</title>
+</head>
+
+<body>
+    <div class="tax-home">
+
+        <div class="main-home">
+            <div class="nav-bar" id="navbar">
+                <div class="container">
+                    <div class="row nav">
+                        <div class="menu col-md-8">
+                            <ul>
+                                <li class="item"><a href="index.html">HOME</a></li>
+                                <li class="item active"><a href="incometax.html">INCOMETAX</a></li>
+                                <li class="item"><a href="gst.html">GST</a></li>
+                                <li class="item"><a href="blog.html">BLOG</a></li>
+                                <li class="item"><a href="#contact">CONTACT US</a></li>
+                            </ul>
+                        </div>
+                        <div class="logo col-md-4">
+                            <img src="assets/icons/CelloTax2@2x.png" alt="CelloTAX.COM">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mob-nav">
+                <div class="hambar" id="hambar"><i class="fa fa-bars"></i></div>
+                <div id="sidebar-menu">
+                    <div class="cros" id="cros"><i class="fa fa-times"></i></div>
+                    <ul>
+                        <li class="item"><a href="index.html">HOME</a></li>
+                        <li class="item active"><a href="incometax.html">INCOMETAX</a></li>
+                        <li class="item"><a href="gst.html">GST</a></li>
+                        <li class="item"><a href="blog.html">BLOG</a></li>
+                        <li class="item"><a href="#contact">CONTACT US</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="hero container">
+                <div class="tax-img">
+                    <img src="./assets/images/incometax.png" alt="">
+                </div>
+                <div class="form-area">
+                    <h3>Register For Plan
+                        <hr class="line1 line">
+                    </h3>
+
+                    <div class="form">
+                        <form action="" method="POST">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="exampleInputname1" placeholder="Enter Your name">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </div>
+                            <a class="btn" href="#">Submit</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="social">
+                <ul>
+                    <li>
+                        <a href="#"><img src="./assets/icons/instagram.png" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="./assets/icons/facebook.png" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="./assets/icons/twitter_1.png" alt=""></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="./assets/icons/linkedin_1.png" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="income-tax">
+        <div class="heading">
+            <h1>INCOME TAX</h1>
+        </div>
+        <div class="tax-card-back">
+            <div class="container">
+                <div class="row pb-4 pt-5">
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - A</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 349.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>Having Salary Income.</li>
+                                        <li>Having Income up to INR 50 Lakh.</li>
+                                        <li>Having One House Property.</li>
+                                        <li>Having Exempt Income.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Having One House Property.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - B</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 799.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>All in Plan-A.</li>
+                                        <li>Having Income up to INR 50 lakh.</li>
+                                        <li>Having Income from Profession.</li>
+                                        <li>Having Income from Freelancer.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Making of Computation of Income.</li>
+                                        <li>Having One House Property.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - C</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 1799.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>All in Plan-B.</li>
+                                        <li>Having Capital Gain Income.</li>
+                                        <li>Having Income from more than One House Property.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Making of Computationof Income.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- </div>
+
+                <div class="row pb-5 pt-4"> -->
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - D</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 2999.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>All in Plan-C</li>
+                                        <li>Having Turnover fro Business not more than INR 2 Crore.</li>
+                                        <li>Having Turnover from from Professionnot more than INR 50 Lakh.</li>
+                                        <li>Having Income Non-Audited.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>Making of Balance (Non-Audited).</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Making of Computation of Income.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - E</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 1399.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>Person Non-Resident in previous year.</li>
+                                        <li>Having Foreign Income.</li>
+                                        <li>Having Income from Other Sources.</li>
+                                        <li>Having Exempt Income.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Making of Computation of Income.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Plan - F</h4>
+                                <div class="amt">
+                                    <h5><i class="fa fa-rupee"></i> 599.<span class="paise">00</span></h5>
+                                    <p>You can pay after your work has done.</p>
+                                </div>
+                                <div class="who">
+                                    <a href="#" class="">Who can Purchase this Plan?</a>
+                                </div>
+                                <div class="content">
+                                    <ol>
+                                        <li>Small Commission Agent.</li>
+                                        <li>Income from Commission up to INR 10 Lakh.</li>
+                                    </ol>
+                                    <h4>Services:</h4>
+                                    <ol>
+                                        <li>Compute of Total Income.</li>
+                                        <li>ITR Filing.</li>
+                                        <li>Making of Computation of Income.</li>
+                                        <li>Maximum Refund Claim.</li>
+                                    </ol>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="buy-btn">Buy</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="contact">
+        <div class="left">
+            <div class="left-item">
+                <div class="sub-logo">
+                    <img src="./assets/icons/Cellotax1.png" alt="CelloTax">
+                </div>
+                <div class="connect">
+                    <div class="mail">
+                        <div class="mail1"><i class="fa fa-envelope"></i> <span>cellotax@gmail.com</span></div>
+                        <div class="mail2"><i class="fa fa-envelope"></i> <span>cellotax@gmail.com</span></div>
+                    </div>
+                    <div class="social">
+                        <p>FOLLOW US</p>
+                        <ul>
+                            <li>
+                                <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-facebook-f"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-whatsapp"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="right-form">
+                <h1>CONTACT US</h1>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleInputname1" placeholder="Enter Your name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleInputnumber1" placeholder="Enter Your Number">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Enter Your Message" rows="3"></textarea>
+                    </div>
+                    <a href="#" class="submit">SUBMIT</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ register_plan.php
@@ -0,0 +1,153 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/main.js">
+    </script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="register-plan">
+        <div class="main-home">
+            <div class="nav-bar" id="navbar">
+                <div class="container">
+                    <div class="row nav">
+                        <div class="menu col-md-8">
+                            <ul>
+                                <li class="item"><a href="index.html">HOME</a></li>
+                                <li class="item"><a href="incometax.html">INCOMETAX</a></li>
+                                <li class="item"><a href="gst.html">GST</a></li>
+                                <li class="item"><a href="blog.html">BLOG</a></li>
+                                <li class="item"><a href="#contact">CONTACT US</a></li>
+                            </ul>
+                        </div>
+                        <div class="logo col-md-4">
+                            <img src="assets/icons/CelloTax2@2x.png" alt="CelloTAX.COM">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mob-nav">
+                <div class="hambar" id="hambar"><i class="fa fa-bars"></i></div>
+                <div id="sidebar-menu">
+                    <div class="cros" id="cros"><i class="fa fa-times"></i></div>
+                    <ul>
+                        <li class="item"><a href="#home">HOME</a></li>
+                        <li class="item"><a href="incometax.html">INCOMETAX</a></li>
+                        <li class="item"><a href="gst.html">GST</a></li>
+                        <li class="item"><a href="blog.html">BLOG</a></li>
+                        <li class="item"><a href="#contact">CONTACT US</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="blog-form">
+            <div class="container">
+                <div class="form-area">
+                    <div class="heading">
+                        <h2 class="text-center">Register For Plan</h2>
+                    </div>
+                    <form>
+                        <div class="row ">
+                            <div class="col">
+                                <input type="text" class="form-control" placeholder="Enter Your Number">
+                            </div>
+                            <div class="col">
+                                <input type="text" class="form-control" placeholder="Enter Your Name">
+                            </div>
+                        </div>
+                        <a href="#">SUBMIT</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="register-social">
+            <ul>
+                <li>
+                    <a href="#"><img src="./assets/icons/instagram.png" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="./assets/icons/facebook.png" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="./assets/icons/twitter_1.png" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="./assets/icons/linkedin_1.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="contact">
+        <div class="left">
+            <div class="left-item">
+                <div class="sub-logo">
+                    <img src="./assets/icons/Cellotax1.png" alt="CelloTax">
+                </div>
+                <div class="connect">
+                    <div class="mail">
+                        <div class="mail1"><i class="fa fa-envelope"></i> <span>cellotax@gmail.com</span></div>
+                        <div class="mail2"><i class="fa fa-envelope"></i> <span>cellotax@gmail.com</span></div>
+                    </div>
+                    <div class="social">
+                        <p>FOLLOW US</p>
+                        <ul>
+                            <li>
+                                <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-facebook-f"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-whatsapp"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="right-form">
+                <h1>CONTACT US</h1>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleInputname1" placeholder="Enter Your name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleInputnumber1" placeholder="Enter Your Number">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Enter Your Message" rows="3"></textarea>
+                    </div>
+                    <a href="#" class="submit">SUBMIT</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
