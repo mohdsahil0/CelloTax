6fc17ea11b47bf6861b0178fb329cc639faf3ccd
FrazileDevelopers <frazile.com@gmail.com>
2021-09-27 07:27:07 +0530

    Register API updated

--- api/reguser.php
+++ api/reguser.php
@@ -1,14 +1,28 @@
 <?php
+
 include('../mysql.php');
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-$sql = "INSERT INTO `users` SET `phone`='',`name`=''";
+$data = json_decode(file_get_contents('php://input'), true);
+
+$phone = $data['phone'];
+$name = $data['name'];
+
+
+$sql = "INSERT INTO `users` SET `phone`='".$phone."',`name`='".$name."'";
 $result = mysqli_query($conn, $sql);
 
-$json_array = array();
+if($result) {
+    // echo 'Data => '.$data;
+    echo json_encode(array('status' => true, 'message' => $name.' Registered Successfully at '.$phone.'!'));
+} else {
+    echo json_encode(array('status' => false, 'message' => 'Server Error!'));
+}
+
 
 
 ?>
