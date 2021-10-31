168d3f0b4afa71082edf2fd30f032dbf6f043d51
Parth <frazilex@gmail.com>
2021-10-31 13:55:04 +0530

    GST API's updated

---
+++ api/getgst.php
@@ -0,0 +1,35 @@
+<?php
+include('../mysql.php');
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Origin: *');
+
+$sql = "SELECT id, amt, planname, content, services FROM gstPlans";
+$result = mysqli_query($conn, $sql);
+
+$json_array = array();
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  $i = 0;
+  while($row = mysqli_fetch_assoc($result)) {
+    // echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - " . $row["phone"]. "<br>";
+    $json_array[$i]['id'] = $row['id'];
+    $json_array[$i]['amt'] = $row['amt'];
+    $json_array[$i]['planname'] = $row['planname'];
+    $json_array[$i]['content'] = str_replace("<li>", "•", str_replace("</li>", "\n", $row['content']));
+    $json_array[$i]['services'] = str_replace("&nbsp;"," ",str_replace("<br>","\n",str_replace("<li>", "•", str_replace("</li>", "\n", $row['services']))));
+    $i++;
+  }
+}
+
+echo json_encode($json_array);
+
+// echo '<pre>';
+// print_r($json_array);
+// echo '</pre>';
+
+
+mysqli_close($conn);
+?>
